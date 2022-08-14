26894c9339c578f8c0e85804bf62c2780d33bd3f
Vinicius Vandré Lopes <vinicius.lopes@tecsidel.com>
2022-08-14 11:13:12 -0400

    Added vrs_utils.py generic functions to be used by all modules.

--- VRS/vrs_gateway.py
+++ VRS/vrs_gateway.py
@@ -1,80 +1,13 @@
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
-from vrs_movie_data import MovieData, MovieKeywords, MovieGenre, MovieCastData
+from flask_restful import Api
+from vrs_movie_data import MovieData
 import vrs_dataset as vrs_ds
 import vrs_ml_model as vrs_ml
 import vrs_recommendation as vrs_rec
+import vrs_utils as vrs_utils
 
 app = Flask(__name__)
 api = Api(app)
-
-
-def get_movie_keywords_from_json(keywords_js):
-    lst_keyw = []
-    for keyword in keywords_js:
-        print(keyword)
-        mv_keyw = MovieKeywords()
-        mv_keyw.id = keyword['id']
-        mv_keyw.name = keyword['name']
-        lst_keyw.append(mv_keyw.__dict__)
-    return str(lst_keyw)
-
-
-def get_movie_genres_from_json(genres_js):
-    lst_genre = []
-    for genre in genres_js:
-        mv_genre = MovieGenre()
-        mv_genre.id = genre['id']
-        mv_genre.name = genre['name']
-        lst_genre.append(mv_genre.__dict__)
-    return str(lst_genre)
-
-
-def get_movie_cast_from_json(cast_js):
-    lst_cast = []
-
-    for cast in cast_js:
-        mv_cast = MovieGenre()
-        mv_cast.id = cast['id']
-        mv_cast.name = cast['name']
-        mv_cast.cast_id = cast['cast_id']
-        mv_cast.character = cast['character']
-        mv_cast.credit_id = cast['credit_id']
-        mv_cast.gender = cast['gender']
-        mv_cast.order = cast['order']
-        lst_cast.append(mv_cast.__dict__)
-    return str(lst_cast)
-
-
-def create_movie_data_from_json(mv, json_content):
-    mv.id = json_content['id']
-    mv.title = json_content['title']
-    mv.overview = json_content['overview']
-    mv.keywords = get_movie_keywords_from_json(json_content['keywords'])
-    mv.genres = get_movie_genres_from_json(json_content['genres'])
-    mv.original_language = json_content['original_language']
-    mv.director = json_content['director']
-    mv.cast = get_movie_cast_from_json(json_content['cast'])
-    mv.status = json_content['status']
-    return mv
-
-
-def create_movies_list_from_df(df):
-    movies_list = []
-    df = df.reset_index()  # make sure indexes pair with number of rows
-    for index, row in df.iterrows():
-        mv = MovieData()
-        mv.id = row['id']
-        mv.title = row['title']
-        mv.overview = row['overview']
-        # mv.keywords = get_movie_keywords_from_json(json_content['keywords'])
-        # mv.genres = get_movie_genres_from_json(json_content['genres'])
-        mv.original_language = row['original_language']
-        mv.director = row['director']
-        # mv.cast = get_movie_cast_from_json(json_content['cast'])
-        mv.status = row['status']
-        movies_list.append(mv.__dict__)
-    return movies_list
 
 
 @app.route('/vrs/dataset/<customer_id>')
@@ -100,7 +33,7 @@ def save_movie(customer_id):
     # if request.method == 'POST':
     content = request.json
     movie = MovieData()
-    movie = create_movie_data_from_json(movie, content)
+    movie = vrs_utils.create_movie_data_from_json(movie, content)
     response = vrs_ds.save(customer_id, movie)
     return response.__dict__
 
@@ -122,7 +55,7 @@ def get_movies_recommendation(customer_id, movie_title):
     movies_list = []
     df = vrs_rec.get_recommendation(customer_id, movie_title)
     if len(df) > 0:
-        movies_list = create_movies_list_from_df(df)
+        movies_list = vrs_utils.create_movies_list_from_df(df)
     return jsonify(movies_list)
 
 
---
+++ VRS/vrs_utils.py
@@ -0,0 +1,70 @@
+from vrs_movie_data import MovieData, MovieKeywords, MovieGenre
+
+
+def get_movie_keywords_from_json(keywords_js):
+    lst_keyw = []
+    for keyword in keywords_js:
+        print(keyword)
+        mv_keyw = MovieKeywords()
+        mv_keyw.id = keyword['id']
+        mv_keyw.name = keyword['name']
+        lst_keyw.append(mv_keyw.__dict__)
+    return str(lst_keyw)
+
+
+def get_movie_genres_from_json(genres_js):
+    lst_genre = []
+    for genre in genres_js:
+        mv_genre = MovieGenre()
+        mv_genre.id = genre['id']
+        mv_genre.name = genre['name']
+        lst_genre.append(mv_genre.__dict__)
+    return str(lst_genre)
+
+
+def get_movie_cast_from_json(cast_js):
+    lst_cast = []
+
+    for cast in cast_js:
+        mv_cast = MovieGenre()
+        mv_cast.id = cast['id']
+        mv_cast.name = cast['name']
+        mv_cast.cast_id = cast['cast_id']
+        mv_cast.character = cast['character']
+        mv_cast.credit_id = cast['credit_id']
+        mv_cast.gender = cast['gender']
+        mv_cast.order = cast['order']
+        lst_cast.append(mv_cast.__dict__)
+    return str(lst_cast)
+
+
+def create_movie_data_from_json(mv, json_content):
+    mv.id = json_content['id']
+    mv.title = json_content['title']
+    mv.overview = json_content['overview']
+    mv.keywords = get_movie_keywords_from_json(json_content['keywords'])
+    mv.genres = get_movie_genres_from_json(json_content['genres'])
+    mv.original_language = json_content['original_language']
+    mv.director = json_content['director']
+    mv.cast = get_movie_cast_from_json(json_content['cast'])
+    mv.status = json_content['status']
+    return mv
+
+
+def create_movies_list_from_df(df):
+    movies_list = []
+    df = df.reset_index()  # make sure indexes pair with number of rows
+    for index, row in df.iterrows():
+        mv = MovieData()
+        mv.id = row['id']
+        mv.title = row['title']
+        mv.overview = row['overview']
+        # mv.keywords = get_movie_keywords_from_json(json_content['keywords'])
+        # mv.genres = get_movie_genres_from_json(json_content['genres'])
+        mv.original_language = row['original_language']
+        mv.director = row['director']
+        # mv.cast = get_movie_cast_from_json(json_content['cast'])
+        mv.status = row['status']
+        movies_list.append(mv.__dict__)
+    return movies_list
+
